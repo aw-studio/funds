b7b4fab4c60f7a6ad9437fe79f949a9588c0e121
Lukas Heller <dev@lukas-heller.de>
2024-10-22 15:30:00 +0200

    update css path

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/editor.js",
-                "modules/Campaign/Views/components/public/public-layout.css",
+                "modules/Campaign/Views/public/public.css",
             ],
             refresh: true,
         }),
