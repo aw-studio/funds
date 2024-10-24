a82544756dc669e230ce2737abd604cd0927be81
Lukas Heller <dev@lukas-heller.de>
2024-10-24 14:23:53 +0200

    refactor: campaigns

---
+++ app-modules/campaign/routes/campaign-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Funds\Campaign\Http\Controllers\CampaignController;
+
+// Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
+// Route::get('/campaigns/create', [CampaignController::class, 'create'])->name('campaigns.create');
+// Route::post('/campaigns', [CampaignController::class, 'store'])->name('campaigns.store');
+// Route::get('/campaigns/{campaign}', [CampaignController::class, 'show'])->name('campaigns.show');
+// Route::get('/campaigns/{campaign}/edit', [CampaignController::class, 'edit'])->name('campaigns.edit');
+// Route::put('/campaigns/{campaign}', [CampaignController::class, 'update'])->name('campaigns.update');
+// Route::delete('/campaigns/{campaign}', [CampaignController::class, 'destroy'])->name('campaigns.destroy');
--- app-modules/campaign/src/Providers/CampaignServiceProvider.php
+++ app-modules/campaign/src/Providers/CampaignServiceProvider.php
@@ -11,9 +11,9 @@ class CampaignServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
         $this->loadRoutesFrom(__DIR__.'/../Http/routes.php');
-        $this->loadViewsFrom(__DIR__.'/../Views', 'campaign');
-        $this->loadJsonTranslationsFrom(__DIR__.'/../lang');
-        $this->loadViewsFrom(__DIR__.'/../Views/public', 'public');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'campaign');
+        $this->loadJsonTranslationsFrom(__DIR__.'/../../resources/lang');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views/public', 'public');
 
         Volt::mount([
             base_path('modules/Campaign/Views/livewire'),
